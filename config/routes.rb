eb3fbe740e80226fd4637c1669fef722a5cb2a01
ArcaneCipher <desjardins.jarp@gmail.com>
2025-03-29 22:26:24 -0400

    Add static About Us page with nav link and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-
-  get 'about/index'
-  root to: 'products#index'
+  get "about", to: "about#index", as: "about"
+  root to: "products#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    post :add_item
+    post :remove_item
   end
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
+    root to: "dashboard#show"
     resources :products, except: [:edit, :update, :show]
   end
 
